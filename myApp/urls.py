1081a92d002d8feed98959b940ebf03d5df8ac5d
SheilaKipchumba <sheilakipchumba43@gmail.com>
2025-12-01 11:13:38 +0300

    Added CRUD functionality for Student and Course models

--- myApp/urls.py
+++ myApp/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('greetings', views.greetings, name='greetings')
+    path('', views.index, name='index'),
+    path('students/', views.students_list, name='students_list'),
+    path('courses/', views.courses_list, name='courses_list'),
+    path('student/<int:student_id>/', views.student_detail, name='student_detail'),
+    path('course/<int:course_id>/', views.course_detail, name='course_detail'),
 ]
--- myApp/views.py
+++ myApp/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Student, Course
 
-# Create your views here.
+# Home page
+def index(request):
+    return render(request, 'myApp/index.html')
 
-def greetings(request):
-    return HttpResponse("Hello, welcome to my app!")
+# List of students
+def students_list(request):
+    students = Student.objects.all()
+    return render(request, 'myApp/students.html', {'students': students})
+
+# List of courses
+def courses_list(request):
+    courses = Course.objects.all()
+    return render(request, 'myApp/courses.html', {'courses': courses})
+
+# Optional: Student details
+def student_detail(request, student_id):
+    student = get_object_or_404(Student, id=student_id)
+    return render(request, 'myApp/student_detail.html', {'student': student})
+
+# Optional: Course details
+def course_detail(request, course_id):
+    course = get_object_or_404(Course, id=course_id)
+    return render(request, 'myApp/course_detail.html', {'course': course})
